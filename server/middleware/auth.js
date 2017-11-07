ead2c59bf36d3eaf03e93006e60124d18586f059
andrewchen44 <andrewchen.c44@gmail.com>
2017-11-06 20:22:51 -0800

    Implementing session, unable to pass test

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,24 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  console.log(req);
+  if (Object.keys(req.cookies).length === 0) {
+    req.session = {
+      hash: 1
+    };
+    
+  } 
+  
+  res.cookies = {shortlyid: '1iuygiuygiuugiuyg'};
+  console.log('res', res);
+  console.log('res.cookies', res.cookies);
+  //if there are no cookies on request
+    //request.session
+  
+  
+  
+  
+  next();
 };
 
 /************************************************************/
