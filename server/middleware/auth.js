b49132e87037869c692426434b6dcb00212299fe
andrewchen44 <andrewchen.c44@gmail.com>
2017-11-07 17:40:22 -0800

    re did as much as we could

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,34 +3,55 @@ const Promise = require('bluebird');
 const session = require('../models/session');
 
 module.exports.createSession = (req, res, next) => {
-  if (Object.keys(req.cookies).length === 0) {
+  var id = req.userId;
+  //if it doesn't have a cookie
+  if (!req.cookies) {
     //create new session and get hash
-    session.create();
-    return new Promise((resolve, reject) => {
-      resolve(session.get({id: 1}));
+    return new Promise ((resolve, reject) => {
+      resolve(session.create());
+    }).then ( (promise) => {
+      return session.get({id: promise.insertId});
     }).then( (obj) => {
       req.session = {};
       req.session['hash'] = obj.hash;
     }).then( () => {
-      res.cookie('shortlyid', 'value');
+      res.cookie('shortlyid', req.session['hash'], {httpOnly: true});
+    }).then ( () => {
+      session.update({hash: req.session['hash']}, {userId: id});
+    }).then ( () => {
       next();
     });
-    
   } else {
-    session.create();
-    return new Promise((resolve, reject) => {
-      resolve(session.get({id: 2})); 
+    //if it has a cookie
+    return new Promise ((resolve, reject) => {
+      resolve(session.create());
+    }).then ( (promise) => {
+      return session.get({id: promise.insertId});
     }).then( (obj) => {
       req.session = {};
       req.session['hash'] = obj.hash;
+    }).then( () => {
+      res.cookie('shortlyid', req.session['hash'], {httpOnly: true});
+    }).then ( () => {
+      return session.get({hash: req.session['hash']});
+      
+    }).then ( (obj) => {
+      session.update({hash: req.session['hash']}, {userId: id});
+      console.log(obj);
+      var newObj = Object.assign({}, obj);
+      var user = Object.assign({}, newObj.user);
+    
+      req.session.user = {};
+      req.session.user.username = user.username;
+      req.session.userId = user.userId;
+    }).then( () => {
       next();
     });
-    
-    
+
   }
   //if there are no cookies on request
     //request.session
-  next();
+
   
   
   
